a7c5268cf30d13d85bc5ad92c567bc2c1166475e
yeeter030 <2002234i@tp.edu.sg>
2022-01-10 21:43:19 +0800

    TrackWithFollowAndRotation

--- PGGE Week 1/Assets/Resources/Scripts/ThirdPersonCamera.cs
+++ PGGE Week 1/Assets/Resources/Scripts/ThirdPersonCamera.cs
@@ -29,6 +29,9 @@ public class ThirdPersonCamera : MonoBehaviour
         //mThirdPersonCamera = new TPCTrack(transform, mPlayer);
         mThirdPersonCamera = new TPCFollowTrackPosition(transform, mPlayer);
 
+        //mThirdPersonCamera = new TPCTrack(transform, mPlayer);
+        //mThirdPersonCamera = new TPCFollowTrackPosition(transform, mPlayer);
+        mThirdPersonCamera = new TPCFollowTrackPositionAndRotation(transform, mPlayer);
     }
 
     // Update is called once per frame
@@ -146,6 +149,26 @@ public class TPCFollowTrackPosition : TPCFollow
 
         // We now call the base class Update method to take care of the
         // position tracking.
+        base.Update();
+    }
+}
+public class TPCFollowTrackPositionAndRotation : TPCFollow
+{
+    public TPCFollowTrackPositionAndRotation(Transform cameraTransform, Transform playerTransform)
+        : base(cameraTransform, playerTransform)
+    {
+    }
+
+    public override void Update()
+    {
+        // We apply the initial rotation to the camera.
+        Quaternion initialRotation = Quaternion.Euler(GameConstants.CameraAngleOffset);
+
+        // Allow rotation tracking of the player
+        // so that our camera rotates when the Player rotates and at the same
+        // time maintain the initial rotation offset.
+        mCameraTransform.rotation = Quaternion.Lerp(mCameraTransform.rotation, mPlayerTransform.rotation * initialRotation,Time.deltaTime * GameConstants.Damping);
+
         base.Update();
     }
 }
